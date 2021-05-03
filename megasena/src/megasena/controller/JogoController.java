2f31206c35ad0729e2769a46c84e0796bafd7bed
ricar <ricar@DESKTOP-SD9LF1Q>
2021-05-02 21:09:25 -0300

    JogoController construção do leitor de arquivo

--- megasena/src/megasena/controller/JogoController.java
+++ megasena/src/megasena/controller/JogoController.java
@@ -1,6 +1,37 @@
 package megasena.controller;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class JogoController {
+	private String arquivo;
+	
 	public JogoController() {
+	}
+	
+	
+	
+	public String getArquivo() {
+		return this.arquivo;
+	}
+
+
+
+	public void setArquivo(String arquivo) {
+		this.arquivo = arquivo;
+	}
+
+
+
+	public void lerArquivo() throws IOException {
+		BufferedReader leitor = new BufferedReader(new FileReader(this.arquivo));
+		String linha = leitor.readLine();
+		while(linha != null) {
+			System.out.println(linha);
+		}
+		leitor.close();
 	}
 }
